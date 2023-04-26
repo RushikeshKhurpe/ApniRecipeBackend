81fc2627be03643ff7e3a63109bc187a0d0d52b6
Rushikesh Khurpe <lit2020049@iiitl.ac.in>
2023-04-27 00:47:57 +0530

    changed get

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (req, res) => {
-  //res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  res.send('hello world');
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  //res.send('hello world');
 });
 
 
